caa57f36861bcb54b5c8a5c6b9d5d04b4ae8e926
Yuganter Pratap <128248352+vansh-py04@users.noreply.github.com>
2026-04-29 11:20:34 +0530

    Upgrade Gemini model to version 2.5

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input,image,prompt):
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content([input,image[0],prompt])
     return response.text
 
